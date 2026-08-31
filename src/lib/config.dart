bc7323e3e8571200f0e36cc8eceb4867b2ce7bc2
masiklas1234 <syma65507@gmail.com>
2026-08-31 15:32:09 +0700

    upload: src/lib/config.dart

---
+++ src/lib/config.dart
@@ -0,0 +1,7 @@
+// ============================================================
+// KONFIGURASI API - LU:NΞX Project
+// ============================================================
+// Ubah URL di sini untuk mengganti endpoint API secara global.
+// ============================================================
+
+const String apiBaseUrl = "http://gabutmaubikinprojectpanel.myserverr.web.id:2033";
