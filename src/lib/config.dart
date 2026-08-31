e58483ad96a4fba671c8d1e9e3723cf442566020
masiklas1234 <syma65507@gmail.com>
2026-08-31 19:10:38 +0700

    cleanup: src/lib/config.dart

--- src/lib/config.dart
+++
@@ -1,7 +0,0 @@
-// ============================================================
-// KONFIGURASI API - LU:NΞX Project
-// ============================================================
-// Ubah URL di sini untuk mengganti endpoint API secara global.
-// ============================================================
-
-const String apiBaseUrl = "http://hanz-cpanel-private.pteroq.biz.id:10874";
