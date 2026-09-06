1dfb8753da4752288b420d4922d4acfcf0155541
masiklas1234 <syma65507@gmail.com>
2026-09-06 12:16:25 +0700

    upload: src/lib/config.dart

---
+++ src/lib/config.dart
@@ -0,0 +1,8 @@
+// ============================================================
+// KONFIGURASI API - NEX PANEL
+// ============================================================
+// Ubah URL di sini untuk mengganti endpoint API secara global.
+// Semua file dart otomatis pakai URL ini — cukup ubah 1 baris.
+// ============================================================
+
+const String apiBaseUrl = "http://hanz-cpanel-private.pteroq.biz.id:10874";
