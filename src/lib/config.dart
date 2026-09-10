bf2fbde3bdb974fff9e8ad3e1b86b1a4a6a3f675
masiklas1234 <syma65507@gmail.com>
2026-09-10 14:17:22 +0700

    upload: src/lib/config.dart

---
+++ src/lib/config.dart
@@ -0,0 +1,7 @@
+// ============================================================
+// KONFIGURASI API - LU:NΞX Project
+// ============================================================
+// Ubah URL di sini untuk mengganti endpoint API secara global.
+// ============================================================
+
+const String apiBaseUrl = "http://rafzzx.surnxuesk.biz.id:2062";
